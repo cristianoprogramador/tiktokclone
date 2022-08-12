afeded0c6d0ffe82a78c36cd661878b355bbf694
Cristiano Silva <cristiano_own@hotmail.com.br>
2022-08-11 23:26:00 -0300

    Projeto Finalizado

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ["cristianosilvadev.com",
+    "lh3.googleusercontent.com"
+  ],
+  }
 }
 
 module.exports = nextConfig
